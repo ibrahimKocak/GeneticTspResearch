d6ac0c44361e998fac91b67cf8da74ff8b5d151e
ibrahim <ibrahimkocak93@gmail.com>
2020-05-20 17:36:13 +0300

    Added getCode function for codename of class.

--- src/main/java/Mutation/IMutation.java
+++ src/main/java/Mutation/IMutation.java
@@ -1,9 +1,11 @@
 package Mutation;
 
+import Model.IAppComponent;
+
 import java.io.Serializable;
 import java.util.List;
 
-public interface IMutation extends Serializable {
+public interface IMutation extends Serializable, IAppComponent {
 
     void infect(List<Integer> list);
 }
