347f2eb3dd2b97d94b942be0b6ab72ade04bd62f
ibrahim <ibrahimkocak93@gmail.com>
2020-05-20 17:34:43 +0300

    Added getCode function for codename of class.

--- src/main/java/PopulationFactory/IPopulationFactory.java
+++ src/main/java/PopulationFactory/IPopulationFactory.java
@@ -1,10 +1,11 @@
 package PopulationFactory;
 
+import Model.IAppComponent;
 import Populations.Population;
 
 import java.io.Serializable;
 
-public interface IPopulationFactory extends Serializable {
+public interface IPopulationFactory extends Serializable, IAppComponent {
 
     Population getPopulation();
 }
